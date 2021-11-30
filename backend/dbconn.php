deff9bd6c6696460e10f93269ebb38c73a42b629
torrente-connie <rose.torrente58@gmail.com>
2021-11-30 16:03:02 +0800

    added new

--- backend/dbconn.php
+++ backend/dbconn.php
@@ -1,16 +1,16 @@
 <?php
 
 function dbConn(){
+	// $servername = "sql305.epizy.com";
+	// $username = "epiz_30468562";
+	// $password = "fE4Tdprz03go";
+	// $db="epiz_30468562_barangayhealth_db";
+
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
+	//$db="mca";
 	$db="barangayhealth_db";
-
-	// $servername = "localhost";
-	// $username = "root";
-	// $password = "";
-	// //$db="mca";
-	// $db="barangayhealth_db";
 
 	static $conn;
 	$conn = mysqli_connect($servername, $username, $password,$db);
