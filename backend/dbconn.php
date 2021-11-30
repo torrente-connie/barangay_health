071db7ed295139a368230c4dd5471204da7543b5
torrente-connie <rose.torrente58@gmail.com>
2021-11-30 14:55:57 +0800

    added new

--- backend/dbconn.php
+++ backend/dbconn.php
@@ -4,8 +4,13 @@ function dbConn(){
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	//$db="mca";
 	$db="barangayhealth_db";
+
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// //$db="mca";
+	// $db="barangayhealth_db";
 
 	static $conn;
 	$conn = mysqli_connect($servername, $username, $password,$db);
